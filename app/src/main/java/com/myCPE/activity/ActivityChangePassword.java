f10fe59a67d2e0bd21b7e766963ff436b347c262
kishansh31 <55207490+kishansh31@users.noreply.github.com>
2020-07-02 20:24:30 +0530

    commit for the following tasks:
    - Worked on the new change password screen with new feature added as show pasword.

--- app/src/main/java/com/myCPE/activity/ActivityChangePassword.java
+++ app/src/main/java/com/myCPE/activity/ActivityChangePassword.java
@@ -9,11 +9,14 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.myCPE.MainActivity;
 import com.myCPE.R;
 import com.myCPE.databinding.ActivityChangePasswordBinding;
+import com.myCPE.databinding.ActivityChangePasswordNewBinding;
 import com.myCPE.model.changepassword.ChangePasswordModel;
 import com.myCPE.utility.AppSettings;
 import com.myCPE.utility.Constant;
@@ -28,17 +31,21 @@ import rx.schedulers.Schedulers;
 public class ActivityChangePassword extends AppCompatActivity {
 
     // Sample test commit..
-    ActivityChangePasswordBinding binding;
+    ActivityChangePasswordNewBinding binding;
     public Context context;
     private APIService mAPIService_new;
     private static final String TAG = ActivityChangePassword.class.getName();
     ProgressDialog progressDialog;
     View view;
 
+    private boolean checkpasswordvisiblestatus = false;
+    private boolean checknewpasswordvisiblestatus = false;
+    private boolean checkconformpasswordvisiblestatus = false;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_change_password);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_change_password_new);
         mAPIService_new = ApiUtilsNew.getAPIService();
         context = ActivityChangePassword.this;
 
@@ -68,6 +75,114 @@ public class ActivityChangePassword extends AppCompatActivity {
                     }
                 }
 
+            }
+        });
+
+        binding.edtOldpassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (binding.edtOldpassword.getRight() - binding.edtOldpassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // your action here
+
+                        if (binding.edtOldpassword.getText().length() > 0) {
+                            if (checkpasswordvisiblestatus == false) {
+                                checkpasswordvisiblestatus = true;
+                                binding.edtOldpassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                                binding.edtOldpassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.password_eye, 0);
+                                binding.edtOldpassword.setSelection(binding.edtOldpassword.length());
+
+                            } else {
+                                checkpasswordvisiblestatus = false;
+                                binding.edtOldpassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                                binding.edtOldpassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.password_eys, 0);
+                                binding.edtOldpassword.setSelection(binding.edtOldpassword.length());
+                            }
+
+                        } else {
+                            Snackbar.make(binding.edtOldpassword, getResources().getString(R.string.validate_password), Snackbar.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        binding.edtNewpassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (binding.edtNewpassword.getRight() - binding.edtNewpassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // your action here
+
+                        if (binding.edtNewpassword.getText().length() > 0) {
+                            if (checknewpasswordvisiblestatus == false) {
+                                checknewpasswordvisiblestatus = true;
+                                binding.edtNewpassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                                binding.edtNewpassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.password_eye, 0);
+                                binding.edtNewpassword.setSelection(binding.edtNewpassword.length());
+
+                            } else {
+                                checknewpasswordvisiblestatus = false;
+                                binding.edtNewpassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                                binding.edtNewpassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.password_eys, 0);
+                                binding.edtNewpassword.setSelection(binding.edtNewpassword.length());
+                            }
+
+                        } else {
+                            Snackbar.make(binding.edtNewpassword, getResources().getString(R.string.validate_password), Snackbar.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        binding.edtConfirmpassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (binding.edtConfirmpassword.getRight() - binding.edtConfirmpassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // your action here
+
+                        if (binding.edtConfirmpassword.getText().length() > 0) {
+                            if (checkconformpasswordvisiblestatus == false) {
+                                checkconformpasswordvisiblestatus = true;
+                                binding.edtConfirmpassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                                binding.edtConfirmpassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.password_eye, 0);
+                                binding.edtConfirmpassword.setSelection(binding.edtConfirmpassword.length());
+
+                            } else {
+                                checkconformpasswordvisiblestatus = false;
+                                binding.edtConfirmpassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                                binding.edtConfirmpassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.password_eys, 0);
+                                binding.edtConfirmpassword.setSelection(binding.edtConfirmpassword.length());
+                            }
+
+                        } else {
+                            Snackbar.make(binding.edtConfirmpassword, getResources().getString(R.string.validate_password), Snackbar.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                }
+                return false;
             }
         });
 
@@ -140,7 +255,7 @@ public class ActivityChangePassword extends AppCompatActivity {
         if (Constant.Trim(binding.edtOldpassword.getText().toString()).isEmpty()) {
 
             binding.edtOldpassword.requestFocus();
-            binding.edtNewpassword.clearFocus();
+            binding.edtConfirmpassword.clearFocus();
             binding.edtConfirmpassword.clearFocus();
 
             Snackbar.make(binding.btnSubmit, getResources().getString(R.string.validate_oldpassword), Snackbar.LENGTH_SHORT).show();
